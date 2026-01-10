032d3d160522d3b781feac0ba796e03b9fa4483c
Csaba Szilagyi <mognoose@gmail.com>
2026-01-10 17:56:40 +0200

    implement message fetching

--- app/components/Game.vue
+++ app/components/Game.vue
@@ -1,13 +1,14 @@
 <template>
 	<div class="session-id">{{session}}</div>
 	<form class="chat" @submit.prevent="submit">
-	<pre>{{ session }}</pre>
+	<pre>{{ gameData }}</pre>
 	<input type="text" v-model="message">
 </form>
 </template>
 
 <script setup lang="ts">
-import { doc, setDoc, arrayUnion } from 'firebase/firestore';
+import { onMounted, ref } from 'vue';
+import { doc, setDoc, arrayUnion, onSnapshot } from 'firebase/firestore';
 
 const { $db } = useNuxtApp()
 
@@ -19,6 +20,18 @@ const props = defineProps({
 })
 
 const message = ref('')
+const gameData = ref(null)
+
+onMounted(() => {
+	const sessionDocRef = doc($db, 'sessions', props.session);
+	onSnapshot(sessionDocRef, (doc) => {
+		if (doc.exists()) {
+			gameData.value = doc.data();
+		} else {
+			gameData.value = { messages: [] };
+		}
+	});
+})
 
 async function submit() {
 	const payload = {
